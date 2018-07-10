efc41aeb5c476677cd5ba0bf1870bd17f6024b3f
King-Benx <kingbenx17@gmail.com>
2018-07-10 05:50:06 +0300

    testing custom functions

---
+++ src/custom/__tests__/CustomFunctions.test.js
@@ -0,0 +1,37 @@
+import CustomFunctions from "../CustomFunctions";
+import { shallow } from 'enzyme'
+
+describe('These are tests for Custom Functions ', () =>{
+    let storedToken;
+    beforeEach(() => {
+        try {
+            storedToken = localStorage.setItem('token','abc');
+        } catch (error) {
+            return null
+        }
+        
+    })
+
+    afterEach(()=>{
+        try {
+            localStorage.clear();
+            localStorage.removeItem('token');
+        } catch (error) {
+            return null
+        }
+        
+    })
+
+    it('has stored a token',() => {
+        expect(CustomFunctions.storeToken('abc')).toEqual(storedToken);
+    })
+
+    it('can get a token',() => {
+        expect(CustomFunctions.getToken()).toEqual('abc');
+    })
+
+    it('can delete token',()=>{
+        expect(CustomFunctions.deleteToken()).toEqual(true);
+    })
+
+})
