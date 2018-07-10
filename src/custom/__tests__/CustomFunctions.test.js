ac5439b1d60f5f9b0aa14fca8dc872681fb6bdb9
King-Benx <kingbenx17@gmail.com>
2018-07-10 09:02:55 +0300

    testing notifications

--- src/custom/__tests__/CustomFunctions.test.js
+++ src/custom/__tests__/CustomFunctions.test.js
@@ -1,11 +1,11 @@
 import CustomFunctions from "../CustomFunctions";
-import { shallow } from 'enzyme'
+import { NotificationManager } from 'react-notifications';
 
 describe('These are tests for Custom Functions ', () =>{
     let storedToken;
     beforeEach(() => {
         try {
-            storedToken = localStorage.setItem('token','abc');
+            storedToken = localStorage.setItem('token','abcdefghijklasdasfdsfasdsdafs');
         } catch (error) {
             return null
         }
@@ -23,19 +23,48 @@ describe('These are tests for Custom Functions ', () =>{
     })
 
     it('has stored a token',() => {
-        expect(CustomFunctions.storeToken('abc')).toEqual(storedToken);
+        expect(CustomFunctions.storeToken('abcdefghijklasdasfdsfasdsdafs')).toEqual(storedToken);
     })
 
     it('can get a token',() => {
-        expect(CustomFunctions.getToken()).toEqual('abc');
+        expect(CustomFunctions.getToken()).toEqual('abcdefghijklasdasfdsfasdsdafs');
     })
 
     it('can delete token',()=>{
         expect(CustomFunctions.deleteToken()).toEqual(true);
     })
 
-    // it('can can authenticate',()=>{
-    //     expect(CustomFunctions.checkAuthentication()).toEqual(false);
-    // })
+    it('can can authenticate',()=>{
+        expect(CustomFunctions.checkAuthentication()).toEqual(true);
+    })
+
+    // Testing the Notifications 
+    it('handles a 200 response',()=>{
+        expect(CustomFunctions.createNotifications(200,'success')).toEqual(NotificationManager.success(message, 'Success'));
+    })
+
+    it('handles a 404 response',()=>{
+        expect(CustomFunctions.createNotifications(404,'unknown')).toEqual(NotificationManager.error('unknown'));
+    })
+
+    it('handles a 500 response',()=>{
+        expect(CustomFunctions.createNotifications(500,'internal server error')).toEqual(NotificationManager.error('internal server error'));
+    })
+
+    it('handles a 201 response',()=>{
+        expect(CustomFunctions.createNotifications(201,'created')).toEqual(NotificationManager.info('created'));
+    })
+
+    it('handles a 400 response',()=>{
+        expect(CustomFunctions.createNotifications(400,'bad request')).toEqual(NotificationManager.warning('bad request'));
+    })
+
+    it('handles a 401 response',()=>{
+        expect(CustomFunctions.createNotifications(401,'unauthorized')).toEqual(NotificationManager.error('unauthorized'));
+    })
+
+    it('handles any response',()=>{
+        expect(CustomFunctions.createNotifications(301,'moved permanently')).toEqual(NotificationManager.error('moved permanently'));
+    })
    
 })
